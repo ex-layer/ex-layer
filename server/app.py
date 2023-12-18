fae0fd8e123533e84cc8d87f476e0c3f005fbf5d
anngo-1 <andngo2004@gmail.com>
2023-12-17 21:52:38 -0800

    server

--- server/app.py
+++ server/app.py
@@ -2,9 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def hello():
-    return 'Hello, World!'
+    print("hi")
+    return "Hello, World!"
+
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=4000)
