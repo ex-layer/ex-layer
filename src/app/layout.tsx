672e94e56c1ec3824ce69551b303482b6b89df39
An <andngo2004@gmail.com>
2023-12-26 20:05:56 +0000

    fix: remove export for font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Jost }  from 'next/font/google'
 import './globals.css'
 
-export const inter = Jost({ subsets: ['latin'] , weight: "400"})
+const inter = Jost({ subsets: ['latin'] , weight: "400"})
 
 export const metadata: Metadata = {
   title: 'ex-layer-demo',
